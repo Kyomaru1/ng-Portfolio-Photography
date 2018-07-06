10f850f46c53f3bea4eb29a9c6a0a70c18c00aaa
Drew Ardner <ardnerdrew@gmail.com>
2018-07-06 00:53:53 -0400

    responsive layout for image cards (prelim)

--- src/app/picture-card/picture-card.component.ts
+++ src/app/picture-card/picture-card.component.ts
@@ -9,11 +9,19 @@ export class PictureCardComponent implements OnInit {
   @Input() photoType : string;
   
   isHeader: string = "";
-
+  overlay_text : string = "";
   constructor(private elementRef : ElementRef) { }
 
   ngOnInit() {
     console.log(this.photoType);
+    switch(this.photoType){
+      case "color":
+        this.overlay_text = "color photography";
+        break;
+      case "blackAndWhite":
+        this.overlay_text = "black and white photography";
+        break;
+    }
   }
 
   convertToHeader(){
